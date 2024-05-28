8718568af8ca105ff35d0d7492498687b778e711
Frankie Mundo <71039075+Frankie59@users.noreply.github.com>
2024-05-27 17:58:30 -0700

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -9,15 +9,11 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
@@ -30,10 +26,6 @@ import latex from "./tech/latex.png";
 import prolog from "./tech/prolog.png";
 import lisp from "./tech/lisp.png";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
 import pepperdine from "./company/pepperdine.png";
 import skip from "./company/skip.png";
 import pep9 from "./company/pep9.png";
@@ -57,26 +49,18 @@ export {
   menu,
   close,
   css,
-  docker,
-  figma,
   git,
   html,
   javascript,
-  mongodb,
   nodejs,
   reactjs,
-  redux,
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
   pepperdine,
   skip,
   pep9,
   pepp,
-  tesla,
   course,
   assembly,
   sortp,
